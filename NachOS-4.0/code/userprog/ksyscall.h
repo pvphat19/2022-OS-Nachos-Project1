2413cec42c85a70e38180bbdf988e14097eab8b0
pvphat19 <64125852+pvphat19@users.noreply.github.com>
2022-02-23 21:53:07 +0700

    SysReadNum
    Writing int SysReadNum() in ksyscall.h

--- NachOS-4.0/code/userprog/ksyscall.h
+++ NachOS-4.0/code/userprog/ksyscall.h
@@ -12,10 +12,26 @@
 #define __USERPROG_KSYSCALL_H__ 
 
 #include "kernel.h"
+#include "synchconsole.h"
+
+/**********************Assist function*********************/
+#define MAX_LENGTH 100
+char buffer[MAX_LENGTH + 6];
+int numBuffer = 0;
+
+int isTerminalChar(char c){
+  if (c == ' ' || c == char(9) || c == '\n') return 1;
+  return -1;
+}
+
+int isDigit(char c){
+  if (c == '1' || c == '2'|| c == '3'|| c == '4'|| c == '5'|| c == '6'
+  || c == '7'|| c == '8'|| c == '9'|| c == '0') return 1;
+  return -1;
+}
 
 
-
-
+/**********************Main function**********************/
 void SysHalt()
 {
   kernel->interrupt->Halt();
@@ -25,6 +41,43 @@ void SysHalt()
 int SysAdd(int op1, int op2)
 {
   return op1 + op2;
+}
+
+int SysReadNum(){
+    numBuffer = 0;
+    int isPositive = 0;
+    while (true){
+      char c = kernel->synchConsoleIn->GetChar();
+      if (isTerminalChar(c) == 1){
+        break;
+      }
+      else{
+
+        if (numBuffer >= MAX_LENGTH){
+          DEBUG(dbgSys, "ReadNum: too long input");
+          break;
+        }
+
+        if (isDigit(c) == -1){
+          if (numBuffer == 0) {
+            if (c == '-') isPositive = 1;
+            else if (c == '+') isPositive = -1;
+            else return 0;
+          }
+          else return 0;
+        }
+        else{
+          numBuffer = numBuffer + 1;
+          buffer[numBuffer-1] = c;
+        }
+      }
+    }
+    int result = 0;
+    for (int i=0;i<numBuffer;i++){
+      int num = int(buffer[i]) - int('0');
+      result = result*10 + num;
+    }
+    return result;
 }
 
 
