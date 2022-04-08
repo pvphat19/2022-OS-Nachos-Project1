2ff75d8ec0f702f8d3069da8edfc74e9345528dc
pvphat19 <pvphat19@apcs.fitus.edu.vn>
2022-04-08 16:44:01 +0700

    open and close file

--- pvp_nachos/NachOS-4.0/code/test/Create.c
+++ pvp_nachos/NachOS-4.0/code/test/Create.c
@@ -1,7 +1,10 @@
 #include "syscall.h"
 
 int main(){
-    char string1[10];
-    Create("test.txt");
+    //char string1[10];
+    //Create("test.txt");
+    int openFileid = Open("test.txt");
+    PrintNum(openFileid);
+    Close("test.txt");
     Halt();
 }
