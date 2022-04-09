e454e4568ed46a6028f6feae0527198703d5bdb4
peternguyen39 <nqktuyen19@apcs.fitus.edu.vn>
2022-04-09 14:55:04 +0700

    test

--- pvp_nachos/NachOS-4.0/code/test/Create.c
+++ pvp_nachos/NachOS-4.0/code/test/Create.c
@@ -2,15 +2,16 @@
 
 int main(){
     //char string1[10];
-    //Create("test.txt");
-    //int openFileid = Open("test.txt");
+    // Create("test.txt");
+    int openFileid = Open("test.txt");
     //PrintNum(openFileid);
-    //char* buffer;
-    //Read(buffer, 10, openFileid);
+    char* buffer;
+    Seek(0, openFileid);
+    Read(buffer, 10, openFileid);
     //PrintNum(Seek(-1,openFileid));
-    Remove("test.txt");
-    //PrintString(buffer);
-    //Write("thang l lam",10,openFileid);
-    //Close(openFileid);
+    // Remove("test.txt");
+    PrintString(buffer);
+    // Write("thang l lam",10,openFileid);
+    Close(openFileid);
     Halt();
 }
